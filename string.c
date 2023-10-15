8a4abcfe7db9b5b2edc6ec7d5efc6701a46521a7
mahmoudmedhat913 <mahmoudmedhat913@gmail.com>
2023-10-15 07:34:31 -0700

    string checker

---
+++ string.c
@@ -0,0 +1,88 @@
+#include "shell.h"
+
+/**
+ * _strlen - return length of string
+ * @str: string
+ * Return: length
+ */
+
+int _strlen(char *str)
+{
+	int i = 0;
+
+	if (!str)
+		return (0);
+	while (*str++)
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * _strcmp - compare 2 strings
+ * @str1: string
+ * @str2: string
+ * Return: 1 if str1>str2 or -1 if str1<str2 or 0 if equal
+ */
+
+int _strcmp(char *str1, char *str2)
+{
+	while (*str1 && *str2)
+	{
+		if (*str1 != *str2)
+			return (*str1 - *str2);
+		str1++;
+		str2++;
+	}
+	if (*str1 == *str2)
+		return (0);
+	if (*str1 < *str2)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (1);
+	}
+}
+
+/**
+ * *startwith - see if needle start with haystack
+ * @haystack: string
+ * @needle: string
+ * Return: address
+ */
+
+char *startwith(const char *haystack, const char *needle)
+{
+	while (*needle)
+	{
+		if (*needle++ != *haystack)
+			return (NULL);
+	}
+	return ((char *)haystack);
+}
+
+/**
+ * *_strcat - concatenate 2 strings
+ * @dest: buffer
+ * @source: buffer
+ * Return: pointer
+ */
+
+char *_strcat(char *dest, char *source)
+{
+	char *x = dest;
+
+	while (*dest)
+	{
+		dest++;
+	}
+	while (*source)
+	{
+		*dest++ = *source++;
+	}
+	*dest = *source;
+	return (x);
+}
