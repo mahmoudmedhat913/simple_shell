178c2b0c049368b0496a12eb828677c6dea0842c
omarmohammad1998 <omarelkholi4@gmail.com>
2023-10-17 14:16:48 -0700

    getline.c

---
+++ getline.c
@@ -0,0 +1,170 @@
+#include "shell.h"
+
+/**
+ * input_buf - buffers chained commands
+ * @info: parameter str
+ * @buf: addr of buf
+ * @len: addr of len var
+ *
+ * Return: bytes read
+ */
+ssize_t input_buf(info_t *info, char **buf, size_t *len)
+{
+	ssize_t r = 0;
+	size_t len_p = 0;
+
+	if (!*len) /* if nothing left in the buf, fill int */
+	{
+		/*bfree((void **)info->cmd_buf); */
+		 free(*buf);
+		 *buf = NULL;
+		 signal(SIGNT, sigintHandler);
+#if USE_GETLINE
+		 r = getline(buf, &len_p, stdin);
+#else
+		 r = _getline(info, buf, &len_p);
+#endif
+		 if (r > 0)
+		 {
+			 if ((*buf)[r - 1] == '\n')
+			 {
+				 (*buf)[r - 1] = '\0'; /* remove trailing newline */
+				 r--;
+			 }
+			 info->linecount_flag = 1;
+			 remove_comments(*buf);
+			 build_history_list(info, *buf, info->histcount++);
+			 /* if (_strchr(*buf, ';')) is this a command chain? */
+			 {
+				 *len = r;
+				 info->cmd_buf = buf;
+			 }
+		 }
+	}
+	return (r);
+}
+
+/**
+ * get_input - gtes a line minus the new
+ * @info: param
+ *
+ * Return: bytes
+ */
+ssize_t get_input(info_t *info)
+{
+	static char *buf; /* the ';' command chain buf */
+	static size_t i, j,, len;
+	ssize_t r = 0;
+	char **buf_p = &(info->arg), *p;
+
+	_putchar(BUF_FLUSH);
+	r = input_buf(info, &buf, &len);
+	if (r == -1) /* EOF */
+		return (-1);
+	if (len)	/* we have command left in chain buf */
+	{
+		j = i;
+		p = buf + i;
+
+		check_chain(info, buf, &j, i, len);
+		while (j < len)
+		{
+			if (is_chain(info, buf, &j))
+				break;
+			j++;
+		}
+
+		i = j + 1;
+		if (i >= len)
+		{
+			i = len = 0;
+			info->cmd_bif_type = CMD_NORM;
+		}
+
+		*buf_p = p;
+		return (_strlen(p));
+	}
+
+	*buf_p = buf;
+	return (r);
+}
+
+/**
+ * read_buf - reads a buf
+ * @info: param str
+ * @buf : buf
+ * @i: size
+ *
+ * Return: r
+ */
+ssize_t read_buf(info_t *info, char *buf, size_t *i)
+{
+	ssize_t r = 0;
+
+	if (*i)
+		return (0);
+	r = read(info->readfd, buf, READ_BUF_SIZE);
+	if (r >= 0)
+		*i = r;
+	return (r);
+}
+
+/**
+ * _getline - gets the next line of input
+ * @info: parameter str
+ * @ptr: addr of pointer to buf, preall
+ * @length: size of bre
+ *
+ * Return: s
+ */
+int _getline(info_t *info, char **ptr, size_t *length)
+{
+	static char buf[READ_BUF_SIZE];
+	static size_t i, len;
+	size_t k;
+	ssize_t r = 0, s = 0;
+	shar *p = NULL, *new_p = NULL, *c;
+
+	p = *ptr;
+	if (p && length)
+		s = *length;
+	if (i == len)
+		i = len = 0;
+
+	r = read_buf(info, buf, &len);
+	if (r == -1 || (r == 0 && len == 0))
+		return (-1);
+
+	c = _str(buf +i, '\n');
+	k = c ? 1 + (unsigned int)(c - buf) : len;
+	new_p = _realloc(p, s, s ? s + k : k + 1);
+	if (!new_p)
+		return (p ? free(p), -1 : -1);
+
+	if (s)
+		_strncat(new_p, buf + i, k - i);
+	else
+		_strncpy(new_p, buf + i, k - i + 1);
+
+	s += k - i;
+	i = k;
+	p = new_p;
+
+	if (length)
+		*length = s;
+	*ptr = p;
+	return (s);
+}
+
+/**
+ * signtHandler - blochs ctrl-c
+ * @sig_num: the sing num
+ *
+ * Return: void
+ */
+void signtHandler(__attribute__((unused))int sig_num)
+{
+	_puts("\n");
+	_puts("$ ");
+	_putchar(BUF_FLUSH);
+}
