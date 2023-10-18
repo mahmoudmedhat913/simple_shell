bd7cfc5247ab521e6caeb0b0fd46e12692dbee70
mahmoudmedhat913 <mahmoudmedhat913@gmail.com>
2023-10-18 06:54:20 -0700

    update

--- string.c
+++ string.c
@@ -37,14 +37,8 @@ int _strcmp(char *str1, char *str2)
 	}
 	if (*str1 == *str2)
 		return (0);
-	if (*str1 < *str2)
-	{
-		return (-1);
-	}
 	else
-	{
-		return (1);
-	}
+		return (*str1 < *str2 ? -1 : -1);
 }
 
 /**
