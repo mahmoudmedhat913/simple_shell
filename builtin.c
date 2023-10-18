74de4a60bbedb0e03ccff479b2e9c770f5b42ced
mahmoudmedhat913 <mahmoudmedhat913@gmail.com>
2023-10-17 23:44:21 -0700

    get enviroment

--- builtin.c
+++ builtin.c
@@ -1,93 +1,91 @@
 #include "shell.h"
 
 /**
- * _myexit - exit the shell
- * @info: struc contain pointtial args. used to maintain constant func proto.
- * Return: exits with a given exit sta
- *		(0) if info.argv[0] != "exit"
+ * exit - exit shell
+ * @info: param struct
+ * Return: exit
  */
-int _myexit(info_t *info)
+int exit(info_t *info)
 {
-	int exitcheck;
+	int check;
 
-	if (info->argvp[1]) /* if there is an exit argument */
+	if (info->argv[1])
 	{
-		exitcheck = _erratoi(info->argv[1]);
-		if (exitcheck == -1)
+		check = erroratoi(info->argv[1]);
+		if (check == -1)
 		{
 			info->status = 2;
-			print_error(info, "Illegal number: ");
-			_eputs(info->argv[1]);
-			_eputchar('\n');
+			printerror(info, "Illegal number: ");
+			_errorputs(info->argv[1]);
+			_errorputchar('\n');
 			return (1);
 		}
-		info->err_num = _erratoi(info->argv[1]);
+		info->errornum = erroratoi(info->argv[1]);
 		return (-2);
 	}
-	info->err_num = -1;
+	info->errornum = -1;
 	return (-2);
 }
 
 /**
- * _mycd - changes the current direct to process
- * @info: struct contain poten arg. used to maintain constants func
- * Return: Always 0
+ * cd - change the current direct to process
+ * @info: param struct
+ * Return: 0
  */
-int _mycd(info_t *info)
+int cd(info_t *info)
 {
 	char *s, *dir, buffer[1024];
-	int chdir_ret;
+	int ret;
 
 	s = getcwd(buffer, 1024);
 	if (!s)
 		_puts("TODO: >>getcwd failure emsg here<<\n");
 	if (!info->argv[1])
 	{
-		dir = _getenv(info, "HOME=");
+		dir = getenv(info, "HOME=");
 		if (!dir)
-			chdir((dir = _getenv(info, "PWD=")) ? dir : "/");
+			ret = chdir((dir = getenv(info, "PWD=")) ? dir : "/");
 		else
-				chdir_ret = chdir(dir);
+			ret = chdir(dir);
 	}
 	else if (_strcmp(info->argv[1], "-") == 0)
 	{
-		if (!_getenv(info, "OLDPWD="))
+		if (!getenv(info, "OLDPWD="))
 		{
 			_puts(s);
 			_putchar('\n');
 			return (1);
 		}
-		_puts(_getenv(info, "OLDPWD=")), _putchar('\n');
-		chdir_ret = /*TODO: what should this be? */
-			chdir((dir = _getenv(info, "OLDPWD=")) ? dir : "/");
+		_puts(getenv(info, "OLDPWD=")), _putchar('\n');
+		ret = chdir((dir = getenv(info, "OLDPWD=")) ? dir : "/");
 	}
 	else
-		chdir_ret = chdir(info->argv[1]);
-	if (chdir_ret == -1)
+		ret = chdir(info->argv[1]);
+	if (ret == -1)
 	{
-		print_error(info, "can't cd to ");
-		_eputs(info->argv[1], _eputchar('\n');)
+		printerror(info, "can't cd to ");
+		_errorputs(info->argv[1], _errorputchar('\n');)
 	}
 	else
 	{
-		_setenv(info, "OLDPWD", _getenv(info, "PWD="));
-		_setenv(info, "PWD", getcwd(buffer, 1024));
+		setenviron(info, "OLDPWD", getenv(info, "PWD="));
+		setenviron(info, "PWD", getcwd(buffer, 1024));
 	}
 	return (0);
 }
 
 /**
- * _myhelp - changes the current dir of the process
- * @info: Structure contain potential args. used to maintain const func prot.
- * Return: Always 0
+ * help - changes the current directory
+ * @info: param struct
+ * Return: 0
  */
-int _myhelp(info_t *info)
+int help(info_t *info)
 {
-	char **arg_ar;
+	char **str;
 
-	arg_ar = info->argv;
+	str = info->argv;
 	_puts("help call works. Function not yet implemented \n");
 	if (0)
-		_puts(*arg_array); /* temp att_unused workround */
+		_puts(*str);
 	return (0);
 }
--- environ.c
+++ environ.c
@@ -1,87 +1,87 @@
 #include "shell.h"
 
 /**
- * _myenv - prints the current env
- * @info: Struct conta poten args. Used to maintain constants fun
+ * env - print the current env
+ * @info: param struct
  * Return: Always 0
  */
-int _myenv(info_t *info)
+int env(info_t *info)
 {
-	print_list_str(info->env);
+	printstringlist(info->env);
 	return (0);
 }
 
 /**
- * _getenv - gets the value of an env vars
- * @info: Struct potent arguments. used to maintain
- * @name: env wav name
+ * *getenv - get value of env vars
+ * @info: param struct
+ * @name: name
  *
- * Return: the value
+ * Return: value
  */
-char *_getenv(info_t *info, const char *name)
+char *getenv(info_t *info, const char *name)
 {
-	list_t *node = info->env;
-	char *p;
+	list_t *n = info->env;
+	char *s;
 
-	while (node)
+	while (n)
 	{
-		p = starts_with(node->str, name);
-		if (p && *p)
-			return (p);
-		node = node->next;
+		s = startwith(n->str, name);
+		if (s && *s)
+			return (s);
+		n = n->next;
 	}
 	return (NULL);
 }
 
 /**
- * _mysetenv - initialize a new env var, or modi an exist
- * @info: Struct conta poten args. used to maintain const fun
- * Return: Always 0
+ * setenv - init a new env var or modifiy one
+ * @info: param struct
+ * Return: 0
  */
-int _mysetenv(info_t *info)
+int setenv(info_t *info)
 {
 	if (info->argc != 3)
 	{
-		_eputs("Incorrect number of arguments\n");
+		_errorputs("Incorrect number of arguments\n");
 		return (1);
 	}
-	if (_setenv(info, info->argv[1], info->argv[2]))
+	if (setenviron(info, info->argv[1], info->argv[2]))
 		return (0);
 	return (1);
 }
 
 /**
- * _myunsetenv - Remove an invi var
- * @info: Struct cont potential arg, Used to ma const func prot
- * Return: Always 0
+ * unsetenv - delete enviroment
+ * @info: param struct
+ * Return: 0
  */
-int _myunsetenv(info_t *info)
+int unsetenv(info_t *info)
 {
 	int i;
 
 	if (info->argc == 1)
 	{
-		_eputs("Too few arguments.\n");
+		_errorputs("Too few arguments.\n");
 		return (1);
 	}
 	for (i = 1; i <= info->argc; i++)
-		_unsetenv(info, info->argv[i]);
+		unsetenviron(info, info->argv[i]);
 
 	return (0);
 }
 
 /**
- * populate_env_list - populates env linked list
- * @info: Struct cont potent args. used to maint const fun pro.
- * Return: Always 0
+ * populateenv - populate env
+ * @info: param struct
+ * Return: 0
  */
-int populate_env_list(info_t *info)
+int populateenv(info_t *info)
 {
-	list_t *node = NULL;
+	list_t *n = NULL;
 	size_t i;
 
 	for (i = 0; environ[i]; i++)
-		add_node_end(&node, environ[i], 0);
-	info->env = node;
+		addendnode(&n, environ[i], 0);
+	info->env = n;
 	return (0);
 }
